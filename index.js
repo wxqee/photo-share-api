1fc124744a1273563f91b76087b924254a50add0
Xiaoqiang Wang <xq21@vip.qq.com>
2020-07-01 21:43:51 +0800

    1 vs *: User vs Photos

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const typeDefs = `
         LANDSCAPE
         GRAPHIC
     }
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+    }
     type Photo {
         id: ID!
         url: String!
         name: String!
         description: String
         category: PhotoCategory!
+        postedBy: User!
     }
     type Query {
         totalPhotos: Int!
@@ -30,7 +37,33 @@ const typeDefs = `
 `
 
 var _id = 0;
-var photos = []
+var users = [
+    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+    { "githubLogin": "gPlake", "name": "Glen Plake" },
+    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+var photos = [
+    {
+        id: "1",
+        name: "Dropping the Heart Chute",
+        description: "The heart chute is one of my favorite chutes",
+        category: "ACTION",
+        githubUser: "gPlake"
+    },
+    {
+        id: "2",
+        name: "Enjoying the sunshine",
+        category: "SELFIE",
+        githubUser: "sSchmidt"
+    },
+    {
+        id: "3",
+        name: "Gunbarrel 25",
+        description: "25 laps on gunbarrel today",
+        category: "LANDSCAPE",
+        githubUser: "sSchmidt"
+    },
+]
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
@@ -47,7 +80,11 @@ const resolvers = {
         }
     },
     Photo: {
-        url: parent => Promise.resolve(`https://example.com/img/${parent.id}.jpg`)
+        url: parent => Promise.resolve(`https://example.com/img/${parent.id}.jpg`),
+        postedBy: parent => users.find(u => u.githubLogin === parent.githubUser)
+    },
+    User: {
+        postedPhotos: parent => photos.filter(p => p.githubUser === parent.githubLogin)
     }
 }
 
