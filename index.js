ed985a967b7ccb83ae540c7e5bc24c67b14bb4ac
Xiaoqiang Wang <xq21@vip.qq.com>
2020-07-01 22:25:51 +0800

    use apollo-server-express replacing apollo-server, and exports /graphql for GraphQL API, /playground for Playground, / for Home

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {ApolloServer} = require('apollo-server')
+const {ApolloServer} = require('apollo-server-express')
+const express = require('express')
+const expressPlayground = require('graphql-playground-middleware-express').default
 const {GraphQLScalarType} = require('graphql')
 
 const typeDefs = `
@@ -119,13 +121,16 @@ const resolvers = {
     })
 }
 
+const app = express()
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
+server.applyMiddleware({ app })
 
-server
-    .listen()
-    .then(({url}) => {
-        console.log(`GraphQL Service running on ${url}`);
-    })
+app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+app.listen({ port: 4000 }, () => {
+    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`);
+})
