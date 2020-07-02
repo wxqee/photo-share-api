6a0934338fc284c2f198c17d62b71c045b8d1c3e
Xiaoqiang Wang <xq21@vip.qq.com>
2020-07-02 17:41:59 +0800

    login from github is available

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const utils = require('../utils')
+
 const Mutation = {
     async postPhoto(parent, args, { db }) {
         const newPhoto = {
@@ -12,6 +14,41 @@ const Mutation = {
         return {
             id: result.insertedId,
             ...newPhoto,
+        }
+    },
+    async githubAuth(parent, { code }, { db }) {
+        let {
+            message,
+            access_token,
+            avatar_url,
+            login,
+            name
+        } = await utils.authorizeWithGithub({
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code,
+        })
+
+        if (message) {
+            throw new Error(message)
+        }
+
+        let latestUserInfo = {
+            name,
+            githubLogin: login,
+            githubToken: access_token,
+            avatar: avatar_url,
+        }
+
+        const {
+            ops: [user]
+        } = await db
+            .collection('users')
+            .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+
+        return {
+            user,
+            token: access_token,
         }
     }
 };
---
+++ utils.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch')
+
+// https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/#2-users-are-redirected-back-to-your-site-by-github
+const requestGithubToken = credentials =>
+    fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(res => res.json())
+        .then(data => {
+            // PS: this code block is useless and sensitive
+            console.debug('access_token response => %o', data);
+            return data
+        })
+        .catch(error => {
+            throw new Error(JSON.stringify(error))
+        })
+
+// https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/#3-use-the-access-token-to-access-the-api
+const requestGithubUserAccount = token =>
+    fetch(`https://api.github.com/user?access_token=${token}`)
+        .then(res => res.json())
+        .catch(error => {
+            throw new Error(JSON.stringify(error))
+        })
+
+const authorizeWithGithub = async (credentials) => {
+    const { access_token } = await requestGithubToken(credentials)
+    const githubUser = await requestGithubUserAccount(access_token)
+    return {
+        ...githubUser,
+        access_token,
+    }
+}
+
+module.exports = {
+    authorizeWithGithub,
+}
